05876d9d58bc6d6d1cf183635d2d94207de5189a
Bozuma Acquaah <bozuma.acquaah@gmail.com>
2018-09-11 00:04:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-var name = "Susan"
-var height = "70"
-if (name === "Susan") {
-    console.log("The name is correct")
- 
-
-} if (height === "70"){
-    console.log("The height is correct")
-}
+var name = "Joe"
+var height = "74"
 
 // Don't worry about this
 module.exports = { name, height
