ceced821ce15b27f42817eb33785510d417d6277
Bozuma Acquaah <bozuma.acquaah@gmail.com>
2018-09-11 00:05:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ if (name === "Susan") {
     console.log("Expected the name to be Susan")
 }
 
-
+if (height === "Susan") {
+    console.log("The height is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
 
 // Don't worry about this
 module.exports = { name, height
